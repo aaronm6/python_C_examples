078f7296e7965361ce5433d49bdd66f158ca6eb4
Aaron Manalaysay <aaronm6@gmail.com>
2023-12-03 16:02:41 -1000

    Added comments

--- ex1/ex1_hello_world.c
+++ ex1/ex1_hello_world.c
@@ -1,16 +1,19 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+// Define the functions in the module
 static PyObject *meth_helloworld(PyObject *self, PyObject *Py_UNUSED(b)) {
 	printf("Hello World!\n");
 	Py_RETURN_NONE;
 }
 
+// Define what is in the module
 static PyMethodDef HelloMethods[] = {
     {"helloworld", meth_helloworld, METH_NOARGS,"No inputs/outputs, just print hello world\n"},
     {NULL, NULL, 0, NULL}
 };
 
+// Define the module
 static struct PyModuleDef hello_module = {
     PyModuleDef_HEAD_INIT,
     "ex1_hello_world",
@@ -19,6 +22,7 @@ static struct PyModuleDef hello_module = {
     HelloMethods
 };
 
+// Initialize the module
 PyMODINIT_FUNC PyInit_ex1_hello_world(void) {
     return PyModule_Create(&hello_module);
 }
