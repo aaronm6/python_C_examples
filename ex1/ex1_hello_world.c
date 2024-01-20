8f8835e842d4d2e5126a3a5a4b1f967f72643a68
Aaron Manalaysay <aaronm6@gmail.com>
2024-01-19 20:37:06 -0800

    Added commenting to the C code

--- ex1/ex1_hello_world.c
+++ ex1/ex1_hello_world.c
@@ -5,36 +5,20 @@
 static PyObject *meth_helloworld(PyObject *self, PyObject *Py_UNUSED(b)) {
 	// Print the message with the native C printf
 	printf("Hello World!\n");
-
+	
+	// For some reason, printf will not work on its own when executed in
+	// a Jupyter notebook (though it will do fine if run from the terminal).
+	// The 'fflush' function forces it to display from a Jupyter notebook.
+	fflush(stdout);
+	
 	// A function that returns nothing has to actually return None, 
 	// with the following command:
-	Py_RETURN_NONE;
-}
-
-static PyObject *meth_helloworld_jupyter(PyObject *self, PyObject *Py_UNUSED(b)) {
-	// printf somehow prints without using stdout; it prints to the terminal only.
-	// Therefore it can't be used in a Jupyter notebook.  This function uses Python's
-	// builtin 'print' function to accomplish this; it is a little more complicated
-	// than 'meth_helloworld' as a consequence.
-	
-	// Get access to the builtin functions
-	PyObject *builtins = PyEval_GetBuiltins();
-	
-	// Find the 'print' function from the builtins
-	PyObject *pyprint = PyDict_GetItemString(builtins, "print");
-	
-	// Call the print function and pass 'Hello World' to it
-	PyObject *a = PyObject_CallOneArg(pyprint, PyUnicode_FromString("Hello World!"));
-	
-	// Decrement the reference count of 'a' since we are done with it now
-	Py_DECREF(a);
 	Py_RETURN_NONE;
 }
 
 // Define what is in the module
 static PyMethodDef HelloMethods[] = {
 	{"helloworld", meth_helloworld, METH_NOARGS,"No inputs/outputs, just print hello world\n"},
-	{"helloworld_jupyter", meth_helloworld_jupyter, METH_NOARGS, "hello world that works in a Jupyter notebook\n"},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -42,7 +26,7 @@ static PyMethodDef HelloMethods[] = {
 static struct PyModuleDef hello_module = {
 	PyModuleDef_HEAD_INIT,
 	"ex1_hello_world",
-	"Provide two functions that print hello world.",
+	"Provide a function that prints hello world.",
 	-1,
 	HelloMethods
 };
