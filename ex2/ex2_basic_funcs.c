fd6fafd783756b1ad9bb16c58d964797bc8991a9
Aaron Manalaysay <aaronm6@gmail.com>
2024-01-21 12:34:02 -0800

    Fixed the stdout flushing and made a notebook for demonstration

--- ex2/ex2_basic_funcs.c
+++ ex2/ex2_basic_funcs.c
@@ -1,44 +1,54 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+// Define module functions
 static PyObject *method_return_long(PyObject *self, PyObject *Py_UNUSED(b)) {
+	// Define the C-type variable. The 'L' after the number just indicates it is a long
 	long n = 262144L;
+	
+	// We have to convert 'n' to a python object
 	PyObject *n_py = PyLong_FromLong(n);
+	
+	// Notice that we are returning a variable of type PyObject, NOT a long int
 	return n_py;
 }
 
 static PyObject *method_accept_1_int_v1(PyObject *self, PyObject *args) {
 	PyObject *a;
-	/* PyArg_ParseTuple unpacks the input "args" a bit.  "O" tells it what to 
-	   do with the input args, in this case, treat it as a python object (NOT 
-	   a C object).  Later, the object is converted to a C long type, which 
-	   can be manipulated in C (after checking its type). */
+	/* PyArg_ParseTuple unpacks the input "args" a bit.  "O", as the 2nd argument 
+	   to PyArgParseTuple, tells it what to do with the input args, in this case, 
+	   treat it as a python object (NOT a C object).  Later, the object is 
+	   converted to a C long type, which can be manipulated in C */
 	if (!PyArg_ParseTuple(args, "O", &a)) {
 		return NULL;
 	}
 	// Verify that the type of a is long, but do nothing if not.
 	if (!Py_IS_TYPE(a, &PyLong_Type)) {
 		printf("Input given must be of [python] type int (a long in C)\n");
+		fflush(stdout);
 	}
-	/*
-	Convert the python object 'a' to a C object 'n'. This function will
-	barf if a's type is not a python int.
-	*/
+	/* Convert the python object 'a' to a C object 'n'. This function will
+	   barf if a's type is not a python int.*/
 	long n = PyLong_AsLong(a);
+	
 	printf("Input given is: %li\n", n);
+	fflush(stdout);
 	Py_RETURN_NONE;
 }
 
 static PyObject *method_accept_1_int_v2(PyObject *self, PyObject *args) {
 	long n;
-	/* PyArg_ParseTuple unpacks the input "args" a bit.  "l" tells it what to 
-	   do with the input args, in this case, convert it directly into a C
-	   object (declared ahead of time).  Because we specify "l", this function
-	   will barf if the given variable is not a long int (python int).*/
+	/* PyArg_ParseTuple unpacks the input "args" a bit.  "l", as the 2nd argument
+	   to PyArgParseTuple, tells it what to do with the input args, in this case, 
+	   convert it directly into a C object (declared ahead of time).  Because we 
+	   specify "l", this function will barf if the given variable is not a long 
+	   int (python int).*/
 	if (!PyArg_ParseTuple(args, "l", &n)) {
 		return NULL;
 	}
+	
 	printf("Input given is: %li\n", n);
+	fflush(stdout);
 	Py_RETURN_NONE;
 }
 
@@ -50,19 +60,26 @@ static PyObject *method_check_type(PyObject *self, PyObject *args) {
 	if (Py_IS_TYPE(a, &PyLong_Type)) {
 		long a_long = PyLong_AsLong(a);
 		printf("Input is %li, of type PyLong\n", a_long);
+		//fflush(stdout);
 	} else if (Py_IS_TYPE(a, &PyFloat_Type)) {
 		double a_double = PyFloat_AsDouble(a);
 		printf("Input is %f, of type PyFloat\n", a_double);
+		//fflush(stdout);
 	} else if (Py_IS_TYPE(a, &PyUnicode_Type)) {
 		const char *a_string = PyUnicode_AsUTF8(a);
 		printf("Input is '%s', of type PyUnicode (i.e. string)\n", a_string);
+		//fflush(stdout);
+	} else if (Py_IS_TYPE(a, &PyTuple_Type)) {
+		printf("Input is of type PyTuple\n");
+		fflush(stdout);
 	} else {
 		printf("Input is of some other type\n");
+		//fflush(stdout);
 	}
-	PyObject *a_type = (PyObject *)Py_TYPE(a);
-	PyObject *type_name = PyObject_GetAttrString(a_type, "__name__");
-	printf("Object's type name is: '%s'\n", PyUnicode_AsUTF8(type_name));
-	
+	PyTypeObject *a_type = Py_TYPE(a);
+	PyObject *type_name = PyObject_GetAttrString((PyObject*)a_type, "__name__");
+	printf("Object's type name is: '%s'\n--\n", PyUnicode_AsUTF8(type_name));
+	fflush(stdout);
 	Py_RETURN_NONE;
 }
 
@@ -82,6 +99,7 @@ static PyObject *method_compare_string(PyObject *self, PyObject *args) {
 	} else {
 		printf("Input '%s' IS NOT the same as '%s'\n", PyUnicode_AsUTF8(a), compare_string);
 	}
+	fflush(stdout);
 	Py_RETURN_NONE;
 }
 
@@ -97,6 +115,7 @@ static PyObject *method_add_two_floats(PyObject *self, PyObject *args) {
 	return PyFloat_FromDouble(c);
 }
 
+// Assemble the list of module functions
 static PyMethodDef FunctionMethods[] = {
 	{"return_long",method_return_long, METH_NOARGS, "Returns a C long, which is a python int. No input."},
 	{"accept_1_int_v1",method_accept_1_int_v1, METH_VARARGS, "Accepts a python int and prints. No output."},
@@ -107,6 +126,7 @@ static PyMethodDef FunctionMethods[] = {
 	{NULL, NULL, 0, NULL}
 };
 
+// Define the module
 static struct PyModuleDef FunctionModule = {
 	PyModuleDef_HEAD_INIT,
 	"ex2_basic_funcs",
@@ -115,6 +135,7 @@ static struct PyModuleDef FunctionModule = {
 	FunctionMethods
 };
 
+// Initiate the module
 PyMODINIT_FUNC PyInit_ex2_basic_funcs(void) {
 	return PyModule_Create(&FunctionModule);
 }
